8db6141a1b2de15b6afb64607f6c58bdc2e5e7e2
Sandro Teleki <88478941+SandorTeleki@users.noreply.github.com>
2022-03-24 13:40:43 +0100

    hotfix: throwerrors

--- utils.js
+++ utils.js
@@ -6,7 +6,7 @@ const throwErrors = (err, res, message) => {
   if (err.name === 'ValidationError') {
     res.status(ERR_INCORRECT_DATA).send({ message: `Ошибка, статус ${ERR_INCORRECT_DATA}. Переданы некорректные данные.` });
   } else if (err.name === 'CastError') {
-    res.status(ERR_NOT_FOUND).send({ message: `Ошибка, статус ${ERR_NOT_FOUND}. ${message}.` });
+    res.status(ERR_INCORRECT_DATA).send({ message: `Ошибка, статус ${ERR_INCORRECT_DATA}. ${message}.` });
   } else {
     res.status(ERR_SERVER_ERROR)
       .send({ message: `Ошибка ${ERR_SERVER_ERROR}. Ошибка сервера.` });
